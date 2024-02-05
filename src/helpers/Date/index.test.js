12f0ece7ceb806c5d1ea7874dcb881f38592ccbf
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-02-05 13:45:09 +0100

    Date: test date

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,18 @@
 /**
- * 
+ *
  */
 
-describe("Date helper", () => {
-    describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
-        });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
-        });
-    });
-})
+import { getMonth } from ".";
 
+describe("Date helper", () => {
+  describe("When getMonth is called", () => {
+    it("the function return janvier for 2022-01-01 as date", () => {
+      const date = new Date("2022-01-01");
+      expect(getMonth(date)).toEqual("janvier");
+    });
+    it("the function return juillet for 2022-07-08 as date", () => {
+      const date = new Date("2022-07-08")
+      expect(getMonth(date)).toEqual("juillet")
+    });
+  });
+});
